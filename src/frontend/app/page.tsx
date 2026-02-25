62075946ab2284bd3bc3e51c0eb106a4aca41b75
Bencso <158219939+bencso@users.noreply.github.com>
2026-02-25 14:52:21 +0100

    Updated EmojiPicker settings

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -4,16 +4,37 @@ import { useWorkSession } from "@/providers/sessionprovider";
 import { useSessionStore } from "@/stores/sessionData";
 import { Box, Button, Tag } from "@chakra-ui/react";
 import { useMemo, useRef, useState } from "react";
-import EmojiPicker, { EmojiClickData, Theme } from 'emoji-picker-react';
+import { EmojiClickData, EmojiStyle, SkinTonePickerLocation, SkinTones, SuggestionMode, Theme } from 'emoji-picker-react';
 import { LuLaugh } from "react-icons/lu";
+import { useColorMode } from "@/components/ui/color-mode";
+import dynamic from "next/dynamic";
 
 export default function Page() {
   const editorRef = useRef<HTMLTextAreaElement>(null);
   const [emojiOpen, setEmojiOpen] = useState<boolean>(false);
   const { getSelectedImageExif } = useSessionStore();
   const { selectedImg } = useWorkSession();
+  const { colorMode } = useColorMode();
   const [tags, setTags] = useState<string[]>([]);
   const tagRegex = /\[([^\]]+)\]/g;
+  const [savedSelection, setSavedSelection] = useState<Range | null>(null);
+
+  const EmojiPicker = dynamic(() => import('emoji-picker-react'), { ssr: false });
+
+  function saveSelection() {
+    const selection = window.getSelection();
+    if (selection && selection.rangeCount > 0) {
+      setSavedSelection(selection.getRangeAt(0).cloneRange());
+    }
+  }
+
+  function restoreSelection() {
+    const selection = window.getSelection();
+    if (savedSelection && selection) {
+      selection.removeAllRanges();
+      selection.addRange(savedSelection);
+    }
+  }
 
   useMemo(() => {
     const exifs = getSelectedImageExif(selectedImg);
@@ -47,6 +68,7 @@ export default function Page() {
   }
 
   function emojiClick(emojiObject: EmojiClickData) {
+    restoreSelection();
     const emoji = emojiObject.emoji;
     const nextNode = document.createTextNode("");
     nextNode.textContent = emoji;
@@ -57,9 +79,7 @@ export default function Page() {
     const range = selection?.getRangeAt(0);
 
     if (range) {
-      range.deleteContents();
       range.insertNode(nextNode)
-      range.insertNode(nextNode);
 
       range.setStartAfter(nextNode);
       range.collapse(true);
@@ -75,16 +95,41 @@ export default function Page() {
     }
   }
 
+  function changeTextTag() {
+    var cleanedText;
+    const selection = window.getSelection();
+    if (!selection?.rangeCount) return;
+
+    const range = selection?.getRangeAt(0);
+    const cursorText = range.startContainer;
+    const inputText = cursorText.textContent;
+    const matches = inputText?.match(tagRegex);
+    if (matches && inputText) {
+      matches.map((match) => {
+        cleanedText = match.replaceAll("[", "").replaceAll("]", "").trim();
+        if (tags.includes(cleanedText)) {
+          const start = inputText.indexOf(match);
+          const end = start + match.length;
+          const matchRange = document.createRange();
+          matchRange.setStart(cursorText, start);
+          matchRange.setEnd(cursorText, end);
+          matchRange.deleteContents();
+
+          createTag(match);
+        }
+      })
+    }
+  }
+
 
   return (
     <Box h="full" px={30}>
       <Box>
-        <Tag.Root onClick={() => { insertTag("tag") }} transition={"all 2ms"} _hover={{ backgroundColor: "teal.800", borderColor: "teal.950" }} size={"xl"} key={1} rounded="full">
-          <Tag.Label>{"tag"}</Tag.Label>
-        </Tag.Root>
         <Box mt={3} display={"flex"} flexWrap={"wrap"} spaceX={2} spaceY={2}>{tags.map((tag, index) => {
           return (
-            <Tag.Root onClick={() => { insertTag(tag) }} transition={"all 2ms"} _hover={{ backgroundColor: "teal.800", borderColor: "teal.950" }} size={"xl"} key={index} rounded="full">
+            <Tag.Root onClick={() => {
+              insertTag(tag);
+            }} colorScheme={"teal"} size={"xl"} key={index} rounded="full">
               <Tag.Label>{tag}</Tag.Label>
             </Tag.Root>
           )
@@ -92,45 +137,24 @@ export default function Page() {
       </Box>
       <Box position={"relative"}>
         <Button mb={2} onClick={() => {
-        setEmojiOpen(!emojiOpen);
-      }}>
-        <LuLaugh />
-      </Button>
-      <Box position={"absolute"}>
-        <EmojiPicker theme={Theme.AUTO} open={emojiOpen} autoFocusSearch onEmojiClick={emojiClick} />
-      </Box>
+          saveSelection();
+          setEmojiOpen(!emojiOpen);
+        }} colorPalette="teal" variant="outline">
+          <LuLaugh />
+        </Button>
+        <Box position={"absolute"}>
+          <EmojiPicker defaultSkinTone={SkinTones.LIGHT} emojiStyle={EmojiStyle.FACEBOOK} lazyLoadEmojis searchPlaceHolder="Keresés" suggestedEmojisMode={SuggestionMode.FREQUENT} skinTonePickerLocation={SkinTonePickerLocation.PREVIEW} theme={colorMode === "dark" ? Theme.DARK : Theme.LIGHT} open={emojiOpen} onEmojiClick={emojiClick} />
+        </Box>
       </Box>
       <Box
-      mt={12}
+        mt={8}
         ref={editorRef}
         contentEditable
         suppressContentEditableWarning
         backgroundColor={"teal.900"}
-        onInput={() => {
-          var cleanedText;
-          const selection = window.getSelection();
-          if (!selection?.rangeCount) return;
-
-          const range = selection?.getRangeAt(0);
-          const cursorText = range.startContainer;
-          const inputText = cursorText.textContent;
-          const matches = inputText?.match(tagRegex);
-          if (matches && inputText) {
-            matches.map((match) => {
-              cleanedText = match.replaceAll("[", "").replaceAll("]", "").trim();
-              if (tags.includes(cleanedText)) {
-                const start = inputText.indexOf(match);
-                const end = start + match.length;
-                const matchRange = document.createRange();
-                matchRange.setStart(cursorText, start);
-                matchRange.setEnd(cursorText, end);
-                matchRange.deleteContents();
-
-                createTag(match);
-              }
-            })
-          }
-        }}
+        p={3}
+        borderRadius={"lg"}
+        onInput={changeTextTag}
       />
     </Box>
   )
